5ad6579da5f471663a93828c93d91e4999210617
dwesterwick <dwesterwick@yahoo.com>
2023-05-21 22:26:33 -0400

    try sorting loot in task

--- bepinex_dev/LateToTheParty/Models/TaskWithTimeLimit.cs
+++ bepinex_dev/LateToTheParty/Models/TaskWithTimeLimit.cs
@@ -1,16 +1,56 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LateToTheParty.Models
 {
-    public class TaskWithTimeLimit
+    public class TaskWithTimeLimit<TResult>
     {
-        public TaskWithTimeLimit()
-        {
+        private CancellationTokenSource cancellationTokenSource;
+        private Task<TResult> task;
 
+        public TaskWithTimeLimit(Func<TResult> action)
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            task = Task.Run(action, cancellationTokenSource.Token);
+        }
+
+        public IEnumerator WaitForTask()
+        {
+            while (isRunning())
+            {
+                Task.Delay(1);
+                yield return null;
+            }
+        }
+
+        public void Abort()
+        {
+            cancellationTokenSource.Cancel();
+        }
+
+        public TResult GetResult()
+        {
+            if (!task.IsCompleted)
+            {
+                throw new InvalidOperationException("The task has not completed.");
+            }
+
+            return task.Result;
+        }
+
+        private bool isRunning()
+        {
+            if (task.Status == TaskStatus.Running)
+            {
+                return false;
+            }
+
+            return false;
         }
     }
 }
