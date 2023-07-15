e0b4621b43e91ac73da35ec610fca0bfe0778e2b
dwesterwick <dwesterwick@yahoo.com>
2023-07-14 22:03:58 -0400

    copy LTTP DLL to config editor output dir

--- bepinex_dev/LTTPConfigEditor/LTTPConfigEditorForm.Designer.cs
+++ bepinex_dev/LTTPConfigEditor/LTTPConfigEditorForm.Designer.cs
@@ -28,20 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LTTPConfigEditorForm));
+            this.mainToolStrip = new System.Windows.Forms.ToolStrip();
+            this.openToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.saveToolStripButton = new System.Windows.Forms.ToolStripButton();
+            this.mainTableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.openConfigDialog = new System.Windows.Forms.OpenFileDialog();
+            this.mainToolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // mainToolStrip
+            // 
+            this.mainToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripButton,
+            this.saveToolStripButton});
+            this.mainToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainToolStrip.Name = "mainToolStrip";
+            this.mainToolStrip.Size = new System.Drawing.Size(884, 25);
+            this.mainToolStrip.TabIndex = 0;
+            this.mainToolStrip.Text = "toolStrip1";
+            // 
+            // openToolStripButton
+            // 
+            this.openToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.openToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripButton.Image")));
+            this.openToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripButton.Name = "openToolStripButton";
+            this.openToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.openToolStripButton.Text = "&Open";
+            this.openToolStripButton.Click += new System.EventHandler(this.openToolStripButton_Click);
+            // 
+            // saveToolStripButton
+            // 
+            this.saveToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.saveToolStripButton.Enabled = false;
+            this.saveToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripButton.Image")));
+            this.saveToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveToolStripButton.Name = "saveToolStripButton";
+            this.saveToolStripButton.Size = new System.Drawing.Size(23, 22);
+            this.saveToolStripButton.Text = "&Save";
+            this.saveToolStripButton.Click += new System.EventHandler(this.saveToolStripButton_Click);
+            // 
+            // mainTableLayoutPanel
+            // 
+            this.mainTableLayoutPanel.ColumnCount = 2;
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 250F));
+            this.mainTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.mainTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainTableLayoutPanel.Location = new System.Drawing.Point(0, 25);
+            this.mainTableLayoutPanel.Name = "mainTableLayoutPanel";
+            this.mainTableLayoutPanel.RowCount = 1;
+            this.mainTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.mainTableLayoutPanel.Size = new System.Drawing.Size(884, 436);
+            this.mainTableLayoutPanel.TabIndex = 1;
+            // 
+            // openConfigDialog
+            // 
+            this.openConfigDialog.DefaultExt = "json";
+            this.openConfigDialog.FileName = "config.json";
+            this.openConfigDialog.Filter = "Late to the Party Configuration|config.json|All Files|*.*";
+            this.openConfigDialog.Title = "Open Late to the Party Configuration";
+            // 
+            // LTTPConfigEditorForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(884, 461);
-            this.Name = "Form1";
+            this.Controls.Add(this.mainTableLayoutPanel);
+            this.Controls.Add(this.mainToolStrip);
+            this.Name = "LTTPConfigEditorForm";
             this.Text = "Late to the Party Config Editor";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LTTPConfigEditorFormClosing);
+            this.mainToolStrip.ResumeLayout(false);
+            this.mainToolStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip mainToolStrip;
+        private System.Windows.Forms.ToolStripButton openToolStripButton;
+        private System.Windows.Forms.ToolStripButton saveToolStripButton;
+        private System.Windows.Forms.TableLayoutPanel mainTableLayoutPanel;
+        private System.Windows.Forms.OpenFileDialog openConfigDialog;
     }
 }
 
--- bepinex_dev/LTTPConfigEditor/LTTPConfigEditorForm.cs
+++ bepinex_dev/LTTPConfigEditor/LTTPConfigEditorForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,44 @@ namespace LTTPConfigEditor
 {
     public partial class LTTPConfigEditorForm : Form
     {
+        private LateToTheParty.Configuration.ModConfig Config;
+
         public LTTPConfigEditorForm()
         {
             InitializeComponent();
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            if (openConfigDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string json = File.ReadAllText(openConfigDialog.FileName);
+                    LateToTheParty.Configuration.ModConfig _config = Newtonsoft.Json.JsonConvert.DeserializeObject<LateToTheParty.Configuration.ModConfig>(json);
+                    Config = _config;
+
+                    saveToolStripButton.Enabled = true;
+                    openToolStripButton.Enabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Error when Reading Configuration", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LTTPConfigEditorFormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("You have unsaved changes. Are you sure you want to quit?", "Unsaved Changes", MessageBoxButtons.YesNo, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button2) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
         }
     }
 }
