087f52aa84b97556bca0290ed603dbaea262ebc5
Dan Westerwick <dwesterwick@yahoo.com>
2026-07-24 15:26:30 -0400

    Added supporting projects

---
+++ Client_FikaSync/LateToThePartyFikaSyncPlugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using Comfort.Common;
+using LateToTheParty.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LateToTheParty
+{
+    [BepInPlugin(ModInfo.GUID + "fikasync", ModInfo.MODNAME + "FikaSync", ModInfo.MOD_VERSION)]
+    internal class LateToThePartyFikaSyncPlugin : BaseUnityPlugin
+    {
+        protected void Awake()
+        {
+            Logger.LogInfo("Loading LateToThePartyFikaSync...");
+
+            Singleton<LoggingUtil>.Create(new LoggingUtil(Logger));
+
+            Singleton<LoggingUtil>.Instance.LogInfo("Loading LateToThePartyFikaSync...done.");
+        }
+    }
+}
